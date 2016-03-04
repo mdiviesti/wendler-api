78f5b78e01dfb404f14af00595276688201b68f3
Michael Diviesti <mediviesti@gmail.com>
2016-03-03 21:03:26 -0600

    Added a 'reps' property to each set for easy display.

--- app.js
+++ app.js
@@ -8,8 +8,8 @@ var app = express();
 app.set('views', './views');
 app.set('view engine', 'jade');
 
-var liftweight = function (weight, val) {
-    return {"orig": (weight *.9) * val, "rounded": Math.round((weight *.9) * val / 5) * 5};
+var liftweight = function (reps, weight, val) {
+    return {"reps": reps, "orig": (weight *.9) * val, "rounded": Math.round((weight *.9) * val / 5) * 5};
 };
 
 var wendler = function (weight) {
@@ -18,33 +18,33 @@ var wendler = function (weight) {
             "week": 1,
             "title": "5-5-5+",
             "description": "2 sets of 5 progressive reps then a 3rd set of 5 to failure",
-            "set1": liftweight(weight, .65),
-            "set2": liftweight(weight, .75),
-            "set3": liftweight(weight, .85)
+            "set1": liftweight(5, weight, .65),
+            "set2": liftweight(5, weight, .75),
+            "set3": liftweight('5+', weight, .85)
         },
         "2": {
             "week": 2,
             "title": "3-3-3+",
             "description": "2 sets of 3 progressive reps then a 3rd set of 3 to failure",
-            "set1": liftweight(weight, .70),
-            "set2": liftweight(weight, .80),
-            "set3": liftweight(weight, .90)
+            "set1": liftweight(3, weight, .70),
+            "set2": liftweight(3, weight, .80),
+            "set3": liftweight('3+', weight, .90)
         },
         "3": {
             "week": 3,
             "title": "5-3-1+",
             "description": "1 set of 5 reps, 1 set of 3 reps, then at least 1 to failure",
-            "set1": liftweight(weight, .75),
-            "set2": liftweight(weight, .85),
-            "set3": liftweight(weight, .95)
+            "set1": liftweight(5, weight, .75),
+            "set2": liftweight(3, weight, .85),
+            "set3": liftweight('1+', weight, .95)
         },
         "4": {
             "week": 4,
             "title": "Deload",
             "description": "3 sets of 5 reps",
-            "set1": liftweight(weight, .40),
-            "set2": liftweight(weight, .50),
-            "set3": liftweight(weight, .60)
+            "set1": liftweight(5, weight, .40),
+            "set2": liftweight(5, weight, .50),
+            "set3": liftweight(5, weight, .60)
         }
     };
 };
